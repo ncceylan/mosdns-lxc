79a726cac873e75df41954f94e86257b108b1552
ncceylan <ncceylan@hotmail.com>
2023-10-07 22:35:46 +0800

    Update Update.sh

--- UpdateTag.sh
+++ UpdateTag.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-wget -qO- -t1 -T2 "https://api.github.com/repos/IrineSistiana/mosdns/releases/latest" | grep "tag_name" | head -n 1 | awk -F ":" '{print $2}' | sed 's/\"//g;s/,//g;s/ //g' > tag
+#
+mosdns_working_dir="/etc/mosdns"
+mkdir -p /tmp/easymosdns && curl https://raw.githubusercontent.com/IceCodeNew/4Share/master/geoip_china/china_ip_list.txt > /tmp/easymosdns/geoip_cn.txt && curl https://raw.githubusercontent.com/Loyalsoldier/v2ray-rules-dat/release/reject-list.txt > /tmp/easymosdns/geosite_category-ads-all.txt && curl https://raw.githubusercontent.com/Loyalsoldier/v2ray-rules-dat/release/proxy-list.txt > /tmp/easymosdns/geosite_geolocation-!cn.txt && curl https://raw.githubusercontent.com/Loyalsoldier/v2ray-rules-dat/release/direct-list.txt > /tmp/easymosdns/geosite_cn.txt && \cp -rf /tmp/easymosdns/*.txt $mosdns_working_dir/ && rm -rf /tmp/easymosdns/* && echo 'update successful'
